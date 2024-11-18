adcec0a33c9b0dd3ff632e72eab3a81487f708ed
Brian Boy <97043252+derseitenschneider@users.noreply.github.com>
2024-11-18 20:09:22 +0100

    feat: Add arguments to fzf repo and shortcuts to tmux.conf

--- scripts/.config/scripts/fzf-repo.sh
+++ scripts/.config/scripts/fzf-repo.sh
@@ -1,17 +1,38 @@
 #!/usr/bin/env bash
-path=$(find  ~/.dotfiles ~/Repositories/work ~/Repositories/personal/ ~/Repositories/02-local/ -maxdepth 1 -mindepth 1 -type d | fzf)
-session_name=$(basename "$path" | tr . _)
-
-
-# Check if session already exists
-# tmux has-session -t "$session_name" 2>/dev/null
-# session_exists=$?
-
-if ! tmux has-session -t "$session_name" 2> /dev/null; then
-    # Start a new session with name in detached mode
-    tmux new-session -s "$session_name" -c "$path" -d 
+if [[ $# -eq 1 ]]; then
+  selected=$1
+else
+  selected=$(find  ~/.dotfiles ~/Repositories/work ~/Repositories/personal/ ~/Repositories/02-local/ -maxdepth 1 -mindepth 1 -type d | fzf)
 fi
 
-# Attach to created session
-# tmux attach-session -t "$session_name" -c "$path"
-tmux switch-client -t "$session_name"
+if [[ -z $selected ]]; then
+  exit 0
+fi
+
+selected_name=$(basename "$selected" | tr . _)
+
+tmux_running=$(pgrep tmux)
+
+if [[ -z $TMUX ]] && [[-z $tmux_running ]]; then
+  tmux new-session -s $selected_name -c $selected
+  exit 0
+fi
+
+if ! tmux has-session -t=$selected_name 2> /dev/null; then
+  tmux new-session -ds $selected_name -c $selected
+fi
+
+tmux switch-client -t $selected_name
+
+# # Check if session already exists
+# # tmux has-session -t "$session_name" 2>/dev/null
+# # session_exists=$?
+#
+# if ! tmux has-session -t "$session_name" 2> /dev/null; then
+#     # Start a new session with name in detached mode
+#     tmux new-session -s "$session_name" -c "$path" -d 
+# fi
+#
+# # Attach to created session
+# # tmux attach-session -t "$session_name" -c "$path"
+# tmux switch-client -t "$session_name"
